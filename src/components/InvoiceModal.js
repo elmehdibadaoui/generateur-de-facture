ed4d651b200e0f64d7dfdac3d0f22957b7abeac0
El Mehdi BADAOUI <mehdibadaoui@outlook.col>
2024-03-03 18:28:33 +0100

    Fix invoice download with a provided html img

--- src/components/InvoiceModal.js
+++ src/components/InvoiceModal.js
@@ -16,41 +16,21 @@ class InvoiceModal extends React.Component {
   }
 
   GenerateInvoice = (event) => {
-    // var entrepriseLogo = this.props.info.entrepriseLogo;
-    // html2canvas(document.querySelector("#invoiceCapture")).then((canvas) => {
-    //   const imgData = canvas.toDataURL('image/png', 1.0);
-    //   // entrepriseLogo = imgData;
-    //   console.log(imgData);
-    //   const pdf = new jsPDF({
-    //     orientation: 'portrait',
-    //     unit: 'pt',
-    //     format: [612, 792]
-    //   });
-    //   pdf.internal.scaleFactor = 1;
-    //   const imgProps= pdf.getImageProperties(imgData);
-    //   const pdfWidth = pdf.internal.pageSize.getWidth();
-    //   const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
-    //   pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
-    //   console.log(entrepriseLogo);
-    //   pdf.addImage(entrepriseLogo, 'PNG', 20, 20, 120, 120); //base64 image, format, x-coordinate, y-coordinate, width, height
-    //   pdf.save('facture-001.pdf');
-    // });
+    html2canvas( document.querySelector("#invoiceCapture"), { logging: true, letterRendering: 1, allowTaint: false, useCORS: true } ).then(canvas => {
+      const imgData = canvas.toDataURL('image/png', 1.0);
+        const pdf = new jsPDF({
+          orientation: 'portrait',
+          unit: 'pt',
+          format: 'a4'
+        });
 
-    const doc = new jsPDF({
-      orientation: 'portrait',
-      format: [612, 792],
-      unit: 'pt',
-    });
-    doc.html(document.querySelector("#invoiceCapture"), {
-        async callback(doc) {
-            await doc.save('facture-001.pdf');
-        },
-    });
-
-
-    // const doc = new jsPDF("p", "mm", "a4");
-    // const width = doc.internal.pageSize.getWidth();
-    // const height = doc.internal.pageSize.getHeight();
+        pdf.internal.scaleFactor = 1;
+        const imgProps= pdf.getImageProperties(imgData);
+        const pdfWidth = pdf.internal.pageSize.getWidth();
+        const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+        pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+        pdf.save('facture-001.pdf');
+    })
   };
 
   render() {
@@ -58,106 +38,9 @@ class InvoiceModal extends React.Component {
       <div>
         <Modal show={this.props.showModal} onHide={this.props.closeModal} size="lg" centered>
           <div id="invoiceCapture">
-          <InvoiceForm isEdit={false} info={this.props.info} />
+            <InvoiceForm isEdit={false} info={this.props.info} />
           </div>
 
-          {/* <div className="d-none" id="invoiceCapture">
-            <div className="d-flex flex-row justify-content-between align-items-start bg-light w-100 p-4">
-              <div className="w-100">
-                <h4 className="fw-bold my-2">{this.props.info.billFrom||'John Uberbacher'}</h4>
-                <h6 className="fw-bold text-secondary mb-1">
-                  Invoice #: {this.props.info.invoiceNumber||''}
-                </h6>
-              </div>
-              <div className="text-end ms-4">
-                <h6 className="fw-bold mt-1 mb-2">Amount&nbsp;Due:</h6>
-                <h5 className="fw-bold text-secondary"> {this.props.currency} {this.props.total}</h5>
-              </div>
-            </div>
-            <div className="p-4">
-              <Row className="mb-4">
-                <Col md={4}>
-                  <div className="fw-bold">Billed From:</div>
-                  <div>{this.props.info.billFrom||''}</div>
-                  <div>{this.props.info.billFromAddress||''}</div>
-                  <div>{this.props.info.billFromEmail||''}</div>
-                </Col>
-                <Col md={4}>
-                  <div className="fw-bold">Billed to:</div>
-                  <div>{this.props.info.billTo||''}</div>
-                  <div>{this.props.info.billToAddress||''}</div>
-                  <div>{this.props.info.billToEmail||''}</div>
-                </Col>
-                <Col md={4}>
-                  <div className="fw-bold mt-2">Date Of Issue:</div>
-                  <div>{this.props.info.dateOfIssue||''}</div>
-                </Col>
-              </Row>
-              <Table className="mb-0">
-                <thead>
-                  <tr>
-                    <th>QTY</th>
-                    <th>DESCRIPTION</th>
-                    <th className="text-end">PRICE</th>
-                    <th className="text-end">AMOUNT</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {this.props.items.map((item, i) => {
-                    return (
-                      <tr id={i} key={i}>
-                        <td style={{width: '70px'}}>
-                          {item.quantity}
-                        </td>
-                        <td>
-                          {item.name} - {item.description}
-                        </td>
-                        <td className="text-end" style={{width: '100px'}}>{this.props.currency} {item.price}</td>
-                        <td className="text-end" style={{width: '100px'}}>{this.props.currency} {item.price * item.quantity}</td>
-                      </tr>
-                    );
-                  })}
-                </tbody>
-              </Table>
-              <Table>
-                <tbody>
-                  <tr>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                    <td>&nbsp;</td>
-                  </tr>
-                  <tr className="text-end">
-                    <td></td>
-                    <td className="fw-bold" style={{width: '100px'}}>SUBTOTAL</td>
-                    <td className="text-end" style={{width: '100px'}}>{this.props.currency} {this.props.subTotal}</td>
-                  </tr>
-                  {this.props.taxAmmount != 0.00 &&
-                    <tr className="text-end">
-                      <td></td>
-                      <td className="fw-bold" style={{width: '100px'}}>TAX</td>
-                      <td className="text-end" style={{width: '100px'}}>{this.props.currency} {this.props.taxAmmount}</td>
-                    </tr>
-                  }
-                  {this.props.discountAmmount != 0.00 &&
-                    <tr className="text-end">
-                      <td></td>
-                      <td className="fw-bold" style={{width: '100px'}}>DISCOUNT</td>
-                      <td className="text-end" style={{width: '100px'}}>{this.props.currency} {this.props.discountAmmount}</td>
-                    </tr>
-                  }
-                  <tr className="text-end">
-                    <td></td>
-                    <td className="fw-bold" style={{width: '100px'}}>TOTAL</td>
-                    <td className="text-end" style={{width: '100px'}}>{this.props.currency} {this.props.total}</td>
-                  </tr>
-                </tbody>
-              </Table>
-              {this.props.info.notes &&
-                <div className="bg-light py-3 px-4 rounded">
-                  {this.props.info.notes}
-                </div>}
-            </div>
-          </div> */}
           <div className="pb-4 px-4">
             <Row>
               <Col md={6}>
